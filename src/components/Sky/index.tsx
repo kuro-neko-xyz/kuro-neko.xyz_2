b797f7773c764eac272babc6a1f38aacbad35820
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-05-12 02:49:41 -0600

    Declared textGeometry property

--- src/components/Sky/index.tsx
+++ src/components/Sky/index.tsx
@@ -1,5 +1,5 @@
 import pressStart2p from "../../assets/fonts/PressStart2P_Regular.json";
-import { FontLoader } from "three/addons/loaders/FontLoader.js";
+import { Font, FontLoader } from "three/addons/loaders/FontLoader.js";
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 import { extend, useFrame } from "@react-three/fiber";
 import { useMemo, useRef } from "react";
@@ -8,17 +8,29 @@ import { CurveModifier, type CurveModifierRef } from "@react-three/drei";
 
 extend({ TextGeometry });
 
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    textGeometry: {
+      args: [
+        string,
+        { font: Font; size: number; height: number; depth: number }
+      ];
+    };
+  }
+}
+
 const Sky = () => {
   const ref = useRef(null) as React.RefObject<Mesh | null>;
   const curveRef = useRef<CurveModifierRef>(null);
+  const radius = 35;
 
   const handlePos = useMemo(
     () => [
-      new Vector3(31, 0, 0),
-      new Vector3(0.707106781 * 31, 0, -0.707106781 * 31),
-      new Vector3(0, 0, -31),
-      new Vector3(-0.707106781 * 31, 0, -0.707106781 * 31),
-      new Vector3(-31, 0, 0),
+      new Vector3(radius, 0, 0),
+      new Vector3(0.707106781 * radius, 0, -0.707106781 * radius),
+      new Vector3(0, 0, -radius),
+      new Vector3(-0.707106781 * radius, 0, -0.707106781 * radius),
+      new Vector3(-radius, 0, 0),
     ],
     []
   );
@@ -41,7 +53,7 @@ const Sky = () => {
       <mesh ref={ref} rotation={[0, 0, 0]}>
         <textGeometry
           args={[
-            "nakadashi means creampie",
+            "nakadashi means creampie nakadashi means creampie nakadashi means creampie nakadashi means creampie nakadashi means creampie",
             { font, size: -1, height: 1, depth: 0 },
           ]}
         />
