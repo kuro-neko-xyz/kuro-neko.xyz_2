466bed831620c44a3e124775a14cb5872360fc9d
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-05-12 02:49:41 -0600

    Added CurveModifier

--- src/components/Sky/index.tsx
+++ src/components/Sky/index.tsx
@@ -2,32 +2,46 @@ import pressStart2p from "../../assets/fonts/PressStart2P_Regular.json";
 import { FontLoader } from "three/addons/loaders/FontLoader.js";
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 import { extend, useFrame } from "@react-three/fiber";
-import { useRef } from "react";
-import type { Mesh } from "three";
+import { useMemo, useRef } from "react";
+import { CatmullRomCurve3, Vector3, type Mesh } from "three";
+import { CurveModifier, type CurveModifierRef } from "@react-three/drei";
 
 extend({ TextGeometry });
 
 const Sky = () => {
   const ref = useRef(null) as React.RefObject<Mesh | null>;
+  const curveRef = useRef<CurveModifierRef>(null);
 
-  useFrame((_, delta) => {
-    if (ref.current) {
-      ref.current.position.x -= delta;
+  const handlePos = useMemo(
+    () => [new Vector3(15, 0, 0), new Vector3(-15, 0, 0)],
+    []
+  );
+
+  const curve = useMemo(
+    () => new CatmullRomCurve3([...handlePos], true, "centripetal"),
+    [handlePos]
+  );
+
+  useFrame(() => {
+    if (curveRef.current) {
+      curveRef.current.moveAlongCurve(-0.001);
     }
   });
 
   const font = new FontLoader().parse(pressStart2p);
 
   return (
-    <mesh ref={ref} position={[0, 0, 0]}>
-      <textGeometry
-        args={[
-          "nakadashi means creampie",
-          { font, size: 1, height: 1, depth: 0 },
-        ]}
-      />
-      <meshStandardMaterial color="#ffffff" />
-    </mesh>
+    <CurveModifier ref={curveRef} curve={curve}>
+      <mesh ref={ref} position={[0, 0, 0]} rotation={[1, 0, 0]}>
+        <textGeometry
+          args={[
+            "nakadashi means creampie",
+            { font, size: 1, height: 1, depth: 0 },
+          ]}
+        />
+        <meshStandardMaterial color="#ffffff" />
+      </mesh>
+    </CurveModifier>
   );
 };
 
