73cc75b906e8f8b4368eadca125b2c30743518ac
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-05-12 02:49:41 -0600

    Initial commit

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,36 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { Mesh } from "three";
+
+interface BoxProps {
+  position: [number, number, number];
+}
+
+const Box = (props: BoxProps) => {
+  const ref = useRef(null) as React.RefObject<Mesh | null>;
+
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  useFrame((_, delta) => {
+    if (ref.current) {
+      ref.current.rotation.x += delta;
+    }
+  });
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
+
+export default Box;
