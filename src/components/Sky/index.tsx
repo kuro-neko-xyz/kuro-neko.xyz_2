16f85c2001e025e379d013073753975d15394dc2
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-06-13 01:28:57 -0600

    XYZ-01: Text Disappears on Re-render

--- src/components/Sky/index.tsx
+++ src/components/Sky/index.tsx
@@ -2,7 +2,7 @@ import pressStart2p from "../../assets/fonts/PressStart2P_Regular.json";
 import { Font, FontLoader } from "three/addons/loaders/FontLoader.js";
 import { TextGeometry } from "three/addons/geometries/TextGeometry.js";
 import { extend, useFrame } from "@react-three/fiber";
-import { useMemo, useRef } from "react";
+import { useRef } from "react";
 import { CatmullRomCurve3, Vector3, type Mesh } from "three";
 import { CurveModifier, type CurveModifierRef } from "@react-three/drei";
 
@@ -19,34 +19,29 @@ declare module "@react-three/fiber" {
   }
 }
 
+const font = new FontLoader().parse(pressStart2p);
+
+const radius = 35;
+
+const handlePos: Vector3[] = [
+  new Vector3(radius, -2.5, 0),
+  new Vector3(0.707106781 * radius, -2.5, -0.707106781 * radius),
+  new Vector3(0, -2.5, -radius),
+  new Vector3(-0.707106781 * radius, -2.5, -0.707106781 * radius),
+  new Vector3(-radius, -2.5, 0),
+];
+
 const Sky = () => {
   const ref = useRef(null) as React.RefObject<Mesh | null>;
   const curveRef = useRef<CurveModifierRef>(null);
-  const radius = 35;
 
-  const handlePos = useMemo(
-    () => [
-      new Vector3(radius, -2.5, 0),
-      new Vector3(0.707106781 * radius, -2.5, -0.707106781 * radius),
-      new Vector3(0, -2.5, -radius),
-      new Vector3(-0.707106781 * radius, -2.5, -0.707106781 * radius),
-      new Vector3(-radius, -2.5, 0),
-    ],
-    []
-  );
-
-  const curve = useMemo(
-    () => new CatmullRomCurve3([...handlePos], true, "centripetal"),
-    [handlePos]
-  );
+  const curve = new CatmullRomCurve3(handlePos, true, "centripetal");
 
   useFrame(() => {
     if (curveRef.current) {
       curveRef.current.moveAlongCurve(0.002);
     }
   });
-
-  const font = new FontLoader().parse(pressStart2p);
 
   return (
     <CurveModifier ref={curveRef} curve={curve}>
